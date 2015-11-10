048ff831c859adfec916dd900c0c53df51890de9
Ali Ukani <ali.ukani@gmail.com>
2015-11-10 16:39:51 -0500

    Add custom markdown loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,8 +1,11 @@
-var path = require('path');
-var webpack = require('webpack');
+var fs = require('fs')
+var path = require('path')
+var webpack = require('webpack')
+
+var __POSTS__ = fs.readdirSync('./src/posts')
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
     './src/index'
@@ -14,13 +17,19 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      '__POSTS__': JSON.stringify(__POSTS__)
+    })
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      { test: /\.md$/, loader: 'md' }
+    ]
   }
-};
+}
