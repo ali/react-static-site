c2bbf1963baec0ba80b91d068fba324d345ef187
Ali Ukani <ali.ukani@gmail.com>
2015-11-10 16:41:03 -0500

    Add blog section

--- src/index.js
+++ src/index.js
@@ -1,42 +1,89 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { createHistory, createMemoryHistory } from 'history'
 import { IndexRoute, Router, Route, Link } from 'react-router'
 
-class Layout extends React.Component {
-  render() {
-    return <div>
-      <h1>My Website</h1>
-      <nav>
-      <Link to="/">Index</Link>
-      <Link to="/about">About</Link>
-      </nav>
+const Navigation = (props) => <nav>
+  <Link to="/">Index</Link>
+  <Link to="/about">About</Link>
+  <Link to="/blog">Blog</Link>
+</nav>
 
-      {this.props.children}
-    </div>
-  }
+const Layout = (props) => <div>
+  <h1>My Website</h1>
+  <Navigation />
+
+  <section id="content">
+    {props.children}
+  </section>
+</div>
+
+const Index = (props) => <div>
+  <h2>Index</h2>
+</div>
+
+const About = (props) => <div>
+  <h2>About</h2>
+</div>
+
+
+const Blog = (props) => <div>
+  <h1>My Blog</h1>
+  <Navigation />
+  {props.children}
+</div>
+
+
+const BlogPostListItem = (props) => <li>
+  <Link to={`/blog/${props.file}`}>{props.post.title}</Link>
+</li>
+
+
+const BlogPostList = (props) => <div>
+  <h2>Posts</h2>
+  <ul>
+    {props.posts.map(file => {
+      const post = require('./posts/' + file)
+      return <BlogPostListItem key={file} file={file} post={post} />
+    })}
+  </ul>
+</div>
+
+
+const BlogIndex = (props) => <div>
+  <h1>Blog.</h1>
+  <p>This is a blog.</p>
+
+  <BlogPostList posts={__POSTS__} />
+</div>
+
+
+const BlogPost = (props) => {
+  const post = require(`./posts/${props.params.file}`)
+  return <div dangerouslySetInnerHTML={post} />
 }
 
-class Index extends React.Component {
-  render() {
-    return <div><h2>Index</h2></div>
-  }
-}
 
-class About extends React.Component {
-  render() {
-    return <div><h2>About</h2></div>
-  }
-}
+const FourOhFour = () => <div><h1>404</h1></div>
+
 
 class Site extends React.Component {
   render() {
-    return <Router>
+    const { history } = this.props
+    return <Router history={history}>
       <Route path="/" component={Layout}>
-      <IndexRoute component={Index} />
-      <Route path="about" component={About} />
+        <IndexRoute component={Index} />
+        <Route path="/about" component={About} />
       </Route>
-      </Router>
+      <Route path="/blog" component={Blog}>
+        <IndexRoute component={BlogIndex} />
+        <Route path=":file" component={BlogPost} />
+      </Route>
+      <Route path="*" component={FourOhFour}/>
+    </Router>
   }
 }
 
-render(<Site />, document.getElementById('root'))
+const history = createHistory()
+
+render(<Site history={history} />, document.getElementById('root'))
---
+++ src/posts/first-post.md
@@ -0,0 +1,9 @@
+---
+title: My first post
+description: I'm writing some stuff.
+pubdate: Tue 10 Nov 2015 05:43:14 AM EST
+---
+
+# First Post
+
+This is my first blog post. Wow.
